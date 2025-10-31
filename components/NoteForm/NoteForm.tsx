3ee42d30101271c404f692c441c52603bb4cec69
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-10-31 14:56:27 +0200

    add form verification

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useId } from 'react';
+import { useId, useState } from 'react';
 import css from './NoteForm.module.css';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createNote } from '@/lib/api';
@@ -8,6 +8,18 @@ import { Note } from '@/types/note';
 import { NOTES_FILTER_CATEGORIES } from '@/lib/constants';
 import { useRouter } from 'next/navigation';
 import { useNoteDraftStore } from '@/lib/store/noteStore';
+import * as Yup from 'yup';
+
+const noteFormSchema = Yup.object().shape({
+  title: Yup.string()
+    .min(3, 'Title too short')
+    .max(50, 'Title too long')
+    .required('Title is required'),
+  content: Yup.string().max(500, 'Content too long'),
+  tag: Yup.string()
+    .oneOf(['Todo', 'Work', 'Personal', 'Meeting', 'Shopping'], 'Invalid tag')
+    .required('Tag is required'),
+});
 
 const NoteForm = () => {
   const fieldId = useId();
@@ -15,6 +27,7 @@ const NoteForm = () => {
   const router = useRouter();
 
   const { draft, setDraft, clearDraft } = useNoteDraftStore();
+  const [errors, setErrors] = useState<Record<string, string>>({});
 
   const handleChange = (
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
@@ -34,9 +47,22 @@ const NoteForm = () => {
     },
   });
 
-  const handleCreateNote = (formData: FormData) => {
+  const handleCreateNote = async (formData: FormData) => {
     const values = Object.fromEntries(formData) as Pick<Note, 'title' | 'content' | 'tag'>;
-    createMutate(values);
+
+    try {
+      await noteFormSchema.validate(values, { abortEarly: false });
+      setErrors({});
+      createMutate(values);
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const validationErrors: Record<string, string> = {};
+        err.inner.forEach((e) => {
+          if (e.path) validationErrors[e.path] = e.message;
+        });
+        setErrors(validationErrors);
+      }
+    }
   };
 
   const handleCancel = () => router.push('/notes/filter/All');
@@ -53,6 +79,7 @@ const NoteForm = () => {
           defaultValue={draft?.title}
           onChange={handleChange}
         />
+        {errors.title && <p className={css.error}>{errors.title}</p>}
       </div>
 
       <div className={css.formGroup}>
@@ -65,6 +92,7 @@ const NoteForm = () => {
           defaultValue={draft?.content}
           onChange={handleChange}
         />
+        {errors.title && <p className={css.error}>{errors.content}</p>}
       </div>
 
       <div className={css.formGroup}>
@@ -82,6 +110,7 @@ const NoteForm = () => {
             </option>
           ))}
         </select>
+        {errors.title && <p className={css.error}>{errors.tag}</p>}
       </div>
 
       <div className={css.actions}>
